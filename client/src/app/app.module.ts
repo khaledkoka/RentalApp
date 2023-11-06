10c83349a3c47956cad97b98b0bc36cba5082ba3
kkokah <khaled.kokah@gmail.com>
2023-11-06 12:03:30 +1100

    Create the housing location component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HousingLocationComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  templateUrl: './housing-location.component.html',
+  styleUrls: ['./housing-location.component.css']
+})
+export class HousingLocationComponent {
+
+}
