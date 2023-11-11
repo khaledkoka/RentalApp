6a80b477b752eb03430daa57158ba5eb180d7c37
kkokah <khaled.kokah@gmail.com>
2023-11-11 12:17:10 +1100

    Add filter feature

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -9,9 +9,22 @@ import { HousingService } from '../services/housing.service';
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
+
   housingService: HousingService = inject(HousingService);
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+  debugger;
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
