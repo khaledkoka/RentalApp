9f75593e8893d0d9cd0174ce4d786b1a72874a1f
kkokah <khaled.kokah@gmail.com>
2023-11-15 10:44:26 +1100

    Re-add swagger [to test in addition to postman]

--- API/Program.cs
+++ API/Program.cs
@@ -4,7 +4,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
