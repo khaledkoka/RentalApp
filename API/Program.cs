0720ba5204248227e0b066a336a9630a862bb15a
kkokah <khaled.kokah@gmail.com>
2023-11-15 15:04:22 +1100

    Create/inject the DataContext class into app services

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,14 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSwaggerGen();
