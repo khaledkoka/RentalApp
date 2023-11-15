b1dbfd79ce023918207973e856d5851122a1798d
kkokah <khaled.kokah@gmail.com>
2023-11-15 16:09:24 +1100

    Create users controller and API endpoints

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // /api/users
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{id}")] // /api/users/2
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            return user;
+        }
+    }
+}
