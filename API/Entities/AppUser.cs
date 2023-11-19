3ee002efbe9f8b24f34c2748cf1803e655a921c0
kkokah <khaled.kokah@gmail.com>
2023-11-19 23:49:01 +1100

    Fix typo in password salt property name

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -10,6 +10,6 @@ namespace API.Entities
         public int Id { get; set; }
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
-        public byte[] SaltHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
