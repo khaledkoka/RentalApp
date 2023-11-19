bbdbcb66aad6ad4d371ba30365dc46cf3844e14e
kkokah <khaled.kokah@gmail.com>
2023-11-19 18:32:53 +1100

    Fix the ssl issue and https connection

--- API/Program.cs
+++ API/Program.cs
@@ -27,7 +27,7 @@ if (app.Environment.IsDevelopment())
 app.UseCors(builder => builder
         .AllowAnyHeader()
         .AllowAnyMethod()
-        .WithOrigins("http://localhost:4200"));
+        .WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
 
--- client/src/app/services/housing.service.ts
+++ client/src/app/services/housing.service.ts
@@ -5,7 +5,7 @@ import { HousingLocation } from '../housinglocation';
   providedIn: 'root'
 })
 export class HousingService {
-  url = 'http://localhost:3000/locations';
+  url = 'https://localhost:3000/locations';
 
   async getAllHousingLocations(): Promise<HousingLocation[]> {
     const data = await fetch(this.url);
