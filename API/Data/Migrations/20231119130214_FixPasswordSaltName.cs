f65d091265bbfdf632e153c06008433c650fe7de
kkokah <khaled.kokah@gmail.com>
2023-11-20 00:03:27 +1100

    Update pass salt column name in database

---
+++ API/Data/Migrations/20231119130214_FixPasswordSaltName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPasswordSaltName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SaltHash",
+                table: "Users",
+                newName: "PasswordSalt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordSalt",
+                table: "Users",
+                newName: "SaltHash");
+        }
+    }
+}
